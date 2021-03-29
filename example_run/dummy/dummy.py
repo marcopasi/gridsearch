2e3640811d2ae5aa1e22d5687b62139868b34e92
Marco Pasi <mfpasi@gmail.com>
2021-03-29 12:30:59 +0200

    Default sleeptime set

--- example_run/dummy/dummy.py
+++ example_run/dummy/dummy.py
@@ -1,6 +1,7 @@
 import sys, time
+SLEEPTIME = 0.1  # seconds
 
-def main(sleeptime=0):
+def main(sleeptime=SLEEPTIME):
     # print(sys.argv)
     time.sleep(sleeptime)
     return sys.argv + [time.ctime()]
